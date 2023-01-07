46934b7faf56ca755018fc8ccdedcc6ba904b035
Yasir Ahmed <code.yasir@gmail.com>
2023-01-07 07:39:20 +0300

    add basic layout

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Outlet } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default Layout;
