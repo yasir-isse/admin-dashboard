96eaa8f2c3bdf86cf8f22b80af9175eb3e6cd1bd
Yasir Ahmed <code.yasir@gmail.com>
2023-01-07 08:38:58 +0300

    import navbar component

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "../../components/Navbar";
+
 const Layout = () => {
   return (
     <Box width="100%" height="100%">
       <Box>
+        <Navbar />
         <Outlet />
       </Box>
     </Box>
