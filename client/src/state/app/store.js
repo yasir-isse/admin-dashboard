e071b459e149ee0c1f18a100dc99d0310aba6705
Yasir Ahmed <code.yasir@gmail.com>
2023-01-11 12:06:52 +0300

    add api reducer

--- client/src/state/app/store.js
+++ client/src/state/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import globalReducer from "../features/globalSlice.js";
 import logger from "redux-logger";
+import { apiSlice } from "../features/api.js";
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: getDefaultMiddleware().concat(logger),
+  middleware: (getDefault) => getDefault().concat(apiSlice.middleware, logger),
 });
 
 export default store;
