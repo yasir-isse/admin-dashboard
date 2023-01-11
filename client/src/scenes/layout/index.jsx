126331578fe5f59e14b90aab37370e178224e9a0
Yasir Ahmed <code.yasir@gmail.com>
2023-01-11 12:05:37 +0300

    fetch & provide data to components && fix navbar flex layout

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,21 +3,28 @@ import { Outlet } from "react-router-dom";
 import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
+import { useSelector } from "react-redux";
+import { useGetUserQuery } from "../../state/features/api";
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
-      <Box>
+      <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
