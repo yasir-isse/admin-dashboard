fc0512c308f905f1a17a7f4d43dc76876a64d1ba
Yasir Ahmed <code.yasir@gmail.com>
2023-01-12 12:16:44 +0300

    create product model

---
+++ server/models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: Number,
+    description: String,
+    category: String,
+    rating: Number,
+    supply: Number,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", ProductSchema);
