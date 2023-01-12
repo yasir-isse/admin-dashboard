9f36790a59f89a097800ef0aca1fb7a12a84caf6
Yasir Ahmed <code.yasir@gmail.com>
2023-01-12 12:16:11 +0300

    configure getProducts controller

---
+++ server/controllers/client.js
@@ -0,0 +1,18 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    const producstWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({ productId: product._id });
+        return { ...product._doc, stat };
+      })
+    );
+    res.status(200).json(producstWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
