9895209af2bb235cb9e8380160427928ce3c96d4
Yasir Ahmed <code.yasir@gmail.com>
2023-01-12 12:13:09 +0300

    add getProducts endpoint query

--- client/src/state/features/api.js
+++ client/src/state/features/api.js
@@ -4,13 +4,17 @@ export const apiSlice = createApi({
   reducerPath: "adminApi",
   // api call to the backend server
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
-  tagTypes: ["User"],
+  tagTypes: ["User", "Products"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       provideTags: ["User"],
     }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
   }),
 });
 
-export const { useGetUserQuery } = apiSlice;
+export const { useGetUserQuery, useGetProductsQuery } = apiSlice;
