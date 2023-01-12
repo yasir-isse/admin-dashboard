a071434039edb49fb2cd38d8950529fcd0b953f4
Yasir Ahmed <code.yasir@gmail.com>
2023-01-12 12:11:02 +0300

    add products route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { themeSettings } from "./theme.js";
 
 import Layout from "./scenes/layout/index";
 import Dashboard from "./scenes/dashboard/index";
+import Products from "./scenes/products/index";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -20,6 +21,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
