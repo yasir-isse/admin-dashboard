a1f930fd78b838cb13dc43b0c95b7289a8998fd9
Yasir Ahmed <code.yasir@gmail.com>
2023-01-14 12:58:02 +0300

    add customers route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { themeSettings } from "./theme.js";
 import Layout from "./scenes/layout/index";
 import Dashboard from "./scenes/dashboard/index";
 import Products from "./scenes/products/index";
+import Customers from "./scenes/customers/index";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -22,6 +23,7 @@ function App() {
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
