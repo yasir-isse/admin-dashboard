4445c05bf16ccdbd3ed6e358df717661b0cca024
Yasir Ahmed <code.yasir@gmail.com>
2023-01-25 14:32:52 +0300

    create transactions get route handler

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -22,6 +23,45 @@ export const getCustomers = async (req, res) => {
   try {
     const customers = await User.find({ role: "user" }).select("-password");
     res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    // info from client: {field:'', sort:'asc || desc'}
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    //format sort info: {field: value} >> ex. {userId:-1}
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
+
+      return sortFormatted;
+    };
+
+    // sort query exist? parse & return a formatted sort query : use default sort
+    const sortQuery = Boolean(sort) ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      //sort options: cost & userID
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(sortQuery)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    const numOfTransactions = await Transaction.countDocuments({
+      name: { $regex: new RegExp(search, "i") },
+    });
+
+    res.status(200).json({ transactions, numOfTransactions });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
