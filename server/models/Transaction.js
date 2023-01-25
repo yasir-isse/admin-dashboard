ba8e3f1e630fa4370848dd95654fc32aff0e4bd5
Yasir Ahmed <code.yasir@gmail.com>
2023-01-25 18:47:56 +0300

    add userId property to transaction model

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const transactionSchema = new mongoose.Schema(
   {
     cost: String,
-    producs: {
+    userId: String,
+    products: {
       type: [mongoose.Types.ObjectId],
       of: String,
     },
