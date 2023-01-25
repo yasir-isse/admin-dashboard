b0262fe617ac1df9aaa1da940f14cb741793d46b
Yasir Ahmed <code.yasir@gmail.com>
2023-01-25 18:55:49 +0300

    add transactions query

--- client/src/state/features/api.js
+++ client/src/state/features/api.js
@@ -4,7 +4,7 @@ export const apiSlice = createApi({
   reducerPath: "adminApi",
   // api call to the backend server
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
-  tagTypes: ["User", "Products", "Customers"],
+  tagTypes: ["User", "Products", "Customers", "Transactions"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -18,8 +18,20 @@ export const apiSlice = createApi({
       query: () => "client/customers",
       providesTags: ["Customers"],
     }),
+    getTransactions: build.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: { page, pageSize, sort, search },
+      }),
+      providesTags: ["Transactions"],
+    }),
   }),
 });
 
-export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } =
-  apiSlice;
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+} = apiSlice;
