4b565ebffcd50640ee3ef99d54c9fb2342483391
Yasir Ahmed <code.yasir@gmail.com>
2023-01-28 22:54:36 +0300

    create admin scene

---
+++ client/src/scenes/admin/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+import { useGetAdminsQuery } from "../../state/features/api";
+import Header from "../../components/Header";
+import columns from "./columnsData";
+import CustomColumnMenu from "../../components/DataGridCustomColumnMenu";
+
+const Admin = () => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetAdminsQuery();
+  console.log(data);
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="ADMINS" subtitle="Managing Admins and List of Admins" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": { border: "none" },
+          "& .MuiDataGrid-cell": { borderBottom: "none" },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[200],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          rows={data || []}
+          getRowId={(row) => row._id}
+          columns={columns}
+          components={{ ColumnMenu: CustomColumnMenu }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Admin;
