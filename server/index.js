fa0326eb7ffd24dbea6f6978963e8d2ca1e2b26e
Yasir Ahmed <code.yasir@gmail.com>
2023-01-30 18:03:16 +0300

    insert affiliateStats data into db

--- server/index.js
+++ server/index.js
@@ -17,12 +17,14 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -62,6 +64,7 @@ mongoose
     ProductStat.insertMany(dataProductStat);
     Transaction.insertMany(dataTransaction);
     OverallStat.insertMany(dataOverallStat);
+    AffiliateStat.insertMany(dataAffiliateStat);
     */
   })
   .catch((error) => console.error(`Connection Failed!\n${error}`));
