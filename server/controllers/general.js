0d16228fb47e78c1d93cff7315f9207031755b97
Yasir Ahmed <code.yasir@gmail.com>
2023-01-30 22:26:38 +0300

    create dashboard endpoint GET route handler

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,11 +1,58 @@
 import User from "../models/User.js";
-
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
     if (!user) res.status(404).json({ message: "User Not Found!" });
     res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (req, res) => {
+  try {
+    //hardcoded data values
+
+    const currentYear = "2021";
+    const currentMonth = "October";
+    const currentDay = "2021-10-03";
+
+    /* Recent Transactions */
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdOn: -1 });
+
+    /* Overall Stats */
+    const overallStats = await OverallStat.find({ year: currentYear });
+
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStats[0];
+
+    const monthStats = overallStats[0].monthlyData.find(
+      ({ month }) => month === currentMonth
+    );
+    const dayStats = overallStats[0].dailyData.find(
+      ({ date }) => date === currentDay
+    );
+
+    res.status(200).json({
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      monthStats,
+      dayStats,
+      transactions,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
