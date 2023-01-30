5830980af6e81869c45a9a6a22e4aaad53fc2d70
Yasir Ahmed <code.yasir@gmail.com>
2023-01-30 22:30:02 +0300

    create statBox component

---
+++ client/src/components/StatBox.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Box, useTheme, Typography } from "@mui/material";
+import FlexBetween from "./FlexBetween";
+
+const StatBox = ({ title, icon, value, increase, description }) => {
+  const theme = useTheme();
+  return (
+    <Box
+      gridColumn="span 2"
+      gridRow="span 1"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      p="1.25rem 1rem"
+      flex="1 1 100%"
+      backgroundColor={theme.palette.background.alt}
+      borderRadius="0.55rem"
+    >
+      <FlexBetween>
+        <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+          {title}
+        </Typography>
+        {icon}
+      </FlexBetween>
+
+      <Typography
+        variant="h3"
+        fontWeight="600"
+        sx={{ color: theme.palette.secondary.light }}
+      >
+        {value}
+      </Typography>
+
+      <FlexBetween gap="1rem">
+        <Typography
+          varint="h5"
+          fontStyle="italic"
+          sx={{ color: theme.palette.secondary.light }}
+        >
+          {increase}
+        </Typography>
+        <Typography>{description}</Typography>
+      </FlexBetween>
+    </Box>
+  );
+};
+
+export default StatBox;
